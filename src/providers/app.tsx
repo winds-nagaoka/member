def26ff1b541f8c05710c73122bf5b27f1887be9
ryo4004 <kato@ryo4004.net>
2023-01-01 01:16:28 +0900

    react-queryのproviderを追加

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,13 +1,17 @@
 import { ReactNode, Suspense } from 'react'
+import { QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from '../library/auth'
+import { queryClient } from '../library/queryClient'
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
     <Suspense fallback={<div>Loading</div>}>
-      <AuthProvider>
-        <BrowserRouter>{children}</BrowserRouter>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <BrowserRouter>{children}</BrowserRouter>
+        </AuthProvider>
+      </QueryClientProvider>
     </Suspense>
   )
 }
