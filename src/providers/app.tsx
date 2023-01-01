c733a3f1a9ed34b0355b9fe91e02b465cbb4b851
ryo4004 <kato@ryo4004.net>
2023-01-01 12:21:26 +0900

    ErrorBoundaryを使う

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,17 +1,30 @@
 import { ReactNode, Suspense } from 'react'
-import { QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider } from 'react-query'
+import { ErrorBoundary } from 'react-error-boundary'
 import { AuthProvider } from '../library/auth'
 import { queryClient } from '../library/queryClient'
+import { Button } from '../components/Form'
+
+const ErrorFallback = () => {
+  return (
+    <>
+      <h2>エラー</h2>
+      <Button onClick={() => window.location.assign(window.location.origin)}>再読み込み</Button>
+    </>
+  )
+}
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
     <Suspense fallback={<div>Loading</div>}>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <BrowserRouter>{children}</BrowserRouter>
-        </AuthProvider>
-      </QueryClientProvider>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <BrowserRouter>{children}</BrowserRouter>
+          </AuthProvider>
+        </QueryClientProvider>
+      </ErrorBoundary>
     </Suspense>
   )
 }
