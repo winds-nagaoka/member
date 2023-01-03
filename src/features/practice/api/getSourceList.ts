c834c9bb55b8a6c7119b3f7c3a4b6b518df73dc6
ryo4004 <kato@ryo4004.net>
2023-01-03 10:03:45 +0900

    参考音源一覧を表示する

--- src/features/practice/api/getSourceList.ts
+++ src/features/practice/api/getSourceList.ts
@@ -3,7 +3,24 @@ import { getSession } from '../../../utilities/session'
 import { fetchApi } from '../../../library/fetch'
 import { APP_API_URL } from '../../../config'
 
-const getSourceList = async () => {
+type Source = {
+  id: string
+  time: number
+  detail: {
+    id: string
+    title: string
+    type: 'source'
+    time: { timestamp: number; date: string; time: string }
+    sourceStatus: true
+    contents: { label: string; music: number[] }[]
+    data: { audio: number; title: string; composer: string; arranger: string; movement: string[]; add: string[] }[]
+  }
+  _id: string
+}
+
+export type SourceListApi = { status: boolean; list: Source[] }
+
+const getSourceList = async (): Promise<SourceListApi> => {
   return await fetchApi(`${APP_API_URL}/api/source`, { session: getSession() })
 }
 
--- src/features/practice/components/SourceList.tsx
+++ src/features/practice/components/SourceList.tsx
@@ -1,10 +1,116 @@
-import { ContentsLoading } from '../../../components/ContentsBox'
+import clsx from 'clsx'
+import { ContentsBox, ContentsLoading, Text, TitleFrame } from '../../../components/ContentsBox'
+import { useStyle } from '../../../utilities/useStyle'
 import { useSourceList } from '../api/getSourceList'
+import { ReactComponent as PlayIcon } from '../../../assets/play-circle.svg'
+import { ReactComponent as NoPlayIcon } from '../../../assets/close-circle.svg'
+import styles from './SourceList.module.scss'
 
 export const SourceList = () => {
+  const pc = useStyle()
   const sourceListQuery = useSourceList()
   if (sourceListQuery.isLoading) {
     return <ContentsLoading />
   }
-  return <></>
+  if (!sourceListQuery.data) {
+    return null
+  }
+  const onClickMusicItem = (id: string, trackNumber: number) => console.log('onClickMusicItem', id, trackNumber)
+  return (
+    <>
+      {sourceListQuery.data.list.length === 0 && (
+        <ContentsBox>
+          <TitleFrame>
+            <Text>みつかりませんでした</Text>
+          </TitleFrame>
+        </ContentsBox>
+      )}
+      {sourceListQuery.data.list.map((sourceItem) => {
+        const { data } = sourceItem.detail
+        return (
+          <ContentsBox key={sourceItem.id}>
+            <div className={styles['source-list']}>
+              <TitleFrame title={sourceItem.detail.title}>
+                <ol>
+                  {sourceItem.detail.contents.map((list, index) => {
+                    const musicList = list.music.map((ml, track) => {
+                      const composer =
+                        'composer' in data[ml] ? (
+                          'arranger' in data[ml] ? (
+                            <span className={styles.composer}>
+                              {data[ml].composer}
+                              {data[ml].composer.match(/民謡/) ? '' : '作曲'}
+                              <span>/</span>
+                              {data[ml].arranger}編曲
+                            </span>
+                          ) : (
+                            <span className={styles.composer}>{data[ml].composer}</span>
+                          )
+                        ) : 'arranger' in data[ml] ? (
+                          <span className={styles.composer}>{data[ml].arranger}編曲</span>
+                        ) : (
+                          ''
+                        )
+                      const additional =
+                        'add' in data[ml] ? (
+                          <ol>
+                            {data[ml].add.map((mv, k) => (
+                              <li key={'a' + sourceItem.id + k}>{mv}</li>
+                            ))}
+                          </ol>
+                        ) : (
+                          ''
+                        )
+                      const movement =
+                        'movement' in data[ml] ? (
+                          <ol>
+                            {data[ml].movement.map((mv, k) => (
+                              <li key={'a' + sourceItem.id + k}>{mv}</li>
+                            ))}
+                          </ol>
+                        ) : (
+                          ''
+                        )
+                      const clickHandler =
+                        'audio' in data[ml] ? () => onClickMusicItem(sourceItem.id, data[ml].audio) : () => {}
+                      const hasAudio = 'audio' in data[ml] ? <PlayIcon /> : <NoPlayIcon />
+                      return (
+                        <li
+                          key={`ml-${sourceItem.id}-${index}-${track}`}
+                          className={clsx(styles.track, { [styles['has-audio']]: 'audio' in data[ml] })}
+                          onClick={clickHandler}
+                        >
+                          <div>{hasAudio}</div>
+                          <div>
+                            <span>{data[ml].title}</span>
+                            {composer}
+                            {additional}
+                            {movement}
+                          </div>
+                        </li>
+                      )
+                    })
+                    return (
+                      <li key={`${sourceItem.id}-${index}`}>
+                        <label
+                          className={clsx(
+                            styles['sticky-label'],
+                            { [styles.other]: !list.label.match(/第[0-9]部/) },
+                            styles[pc]
+                          )}
+                        >
+                          {list.label}
+                        </label>
+                        <ol>{musicList}</ol>
+                      </li>
+                    )
+                  })}
+                </ol>
+              </TitleFrame>
+            </div>
+          </ContentsBox>
+        )
+      })}
+    </>
+  )
 }
