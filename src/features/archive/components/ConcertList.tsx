d5b30cc321ab540391c3bec307c35cb106531177
ryo4004 <kato@ryo4004.net>
2023-01-05 00:37:40 +0900

    演奏会一覧を表示する

--- src/features/archive/components/ConcertList.tsx
+++ src/features/archive/components/ConcertList.tsx
@@ -1,5 +1,9 @@
+import { Link } from 'react-router-dom'
+import clsx from 'clsx'
 import { ContentsBox, ContentsLoading } from '../../../components/ContentsBox'
 import { useConcertList } from '../api/getConcertList'
+import { ReactComponent as RightIcon } from '../../../assets/right.svg'
+import styles from './ConcertList.module.scss'
 
 export const ConcertList = () => {
   const concertListQuery = useConcertList()
@@ -9,5 +13,26 @@ export const ConcertList = () => {
   if (!concertListQuery.data) {
     return null
   }
-  return <ContentsBox>演奏会一覧</ContentsBox>
+
+  const concertList = [...concertListQuery.data.list].reverse()
+  return (
+    <ContentsBox>
+      <div className={styles['archive-list']}>
+        {concertList.map((concertItem) => {
+          const { detail } = concertItem
+          return (
+            <Link key={concertItem.id} to={`/archive/overview/${concertItem.id}`} className={styles['concert-item']}>
+              <div className={clsx(styles.info, styles[detail.type])}>
+                <span>{detail.title}</span>
+                <span className={styles.date}>{detail.time.date}</span>
+              </div>
+              <div className={styles.icon}>
+                <RightIcon />
+              </div>
+            </Link>
+          )
+        })}
+      </div>
+    </ContentsBox>
+  )
 }
