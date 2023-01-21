af5a8224466094dbbdc6b15295b18fc4358cdc06
ryo4004 <kato@ryo4004.net>
2023-01-21 23:10:27 +0900

    楽譜管理箱を一覧表示する

--- src/features/score/components/BoxList.tsx
+++ src/features/score/components/BoxList.tsx
@@ -1,3 +1,51 @@
+import clsx from 'clsx'
+import { ContentsBox, ContentsLoading } from '../../../components/ContentsBox'
+import { useBoxList } from '../api/getBoxList'
+import styles from './BoxList.module.scss'
+import { ReactComponent as BoxIcon } from '../../../assets/box.svg'
+
 export const BoxList = () => {
-  return <>BoxList</>
+  const boxListQuery = useBoxList()
+
+  if (boxListQuery.isLoading) {
+    return <ContentsLoading />
+  }
+
+  if (!boxListQuery.data) {
+    return null
+  }
+
+  return (
+    <ContentsBox>
+      <div className={styles['score-box-list']}>
+        {boxListQuery.data.list.map((box) => {
+          if (!box.status) {
+            return null
+          }
+
+          return (
+            <div
+              key={`score-box-${box._id}`}
+              className={clsx(styles['score-box'], { [styles.disabled]: !box.status })}
+              onClick={() => {
+                // this.props.setDisplayBoxModal(true, each)
+              }}
+            >
+              <div className="label">
+                <span>{box.label}</span>
+              </div>
+              <BoxIcon />
+              <div className={clsx(styles.locate, { [styles.disabled]: !box.locate })}>
+                <span>{box.locate}</span>
+              </div>
+            </div>
+          )
+        })}
+        {boxListQuery.data.list.length % 3 !== 0 &&
+          (3 - (boxListQuery.data.list.length % 3) === 1 ? [''] : ['', '']).map((_, index) => {
+            return <div key={`blank-${index}`} className={clsx(styles['score-box'], styles.blank)}></div>
+          })}
+      </div>
+    </ContentsBox>
+  )
 }
