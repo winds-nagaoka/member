6df8283f2b4ac430be966294661eee7aa47b2874
ryo4004 <kato@ryo4004.net>
2023-01-22 01:16:35 +0900

    楽譜管理箱の場所変更処理を修正

--- src/features/score/api/updateScoreBox.ts
+++ src/features/score/api/updateScoreBox.ts
@@ -1,7 +1,11 @@
 import { useMutation } from 'react-query'
 import { SCORE_API_URL } from '../../../config'
 import { fetchApi } from '../../../library/fetch'
+import { queryClient } from '../../../library/queryClient'
+import { useNotificationStore } from '../../../stores/notification'
+import { useScoreBoxModalStore } from '../../../stores/scoreBoxModal'
 import { getSession } from '../../../utilities/session'
+import { BoxListApi } from './getBoxList'
 
 type UpdateScoreBoxData = {
   id: string
@@ -13,7 +17,27 @@ const updateScoreBox = async (updateScoreBoxData: UpdateScoreBoxData) => {
 }
 
 export const useUpdateScoreBox = () => {
+  const { addNotification } = useNotificationStore()
+  const { onClose } = useScoreBoxModalStore()
+
   return useMutation({
+    onMutate: async (updateScoreBoxData: UpdateScoreBoxData) => {
+      await queryClient.cancelQueries(['boxList'])
+      const previousBoxList = queryClient.getQueryData<BoxListApi>(['boxList'])
+      const newBoxList = previousBoxList?.list.map((box) => {
+        return box._id !== updateScoreBoxData.id ? box : { ...box, locate: updateScoreBoxData.locate }
+      })
+      queryClient.setQueryData(['boxList'], { ...previousBoxList, list: newBoxList })
+      return { previousBoxList }
+    },
+    onError: () => {
+      console.log('onError')
+    },
+    onSuccess: () => {
+      onClose()
+      queryClient.invalidateQueries(['boxList'])
+      addNotification('楽譜管理箱の場所を変更しました')
+    },
     mutationFn: async (updateScoreBoxData: UpdateScoreBoxData) => await updateScoreBox(updateScoreBoxData),
   })
 }
