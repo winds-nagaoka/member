4aadf1fb0d3fae7b84eecda4c2b9f1558c8dbbe4
ryo4004 <kato@ryo4004.net>
2023-01-24 00:49:57 +0900

    Alertコンポーネントを追加

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,35 @@
+import styles from './Alert.module.scss'
+
+export const Alert = ({
+  title,
+  message,
+  confirmButtonLabel = '確認',
+  cancelButtonLabel = 'キャンセル',
+  onConfirm,
+  onClose,
+}: {
+  title: string
+  message: string
+  confirmButtonLabel?: string
+  cancelButtonLabel?: string
+  onConfirm: () => void
+  onClose: () => void
+}) => {
+  return (
+    <div className={styles.alert}>
+      <h1>{title}</h1>
+      <p>{message}</p>
+      <div className={styles['button-group']}>
+        <button onClick={onClose}>{cancelButtonLabel}</button>
+        <button
+          onClick={() => {
+            onConfirm()
+            onClose()
+          }}
+        >
+          {confirmButtonLabel}
+        </button>
+      </div>
+    </div>
+  )
+}
