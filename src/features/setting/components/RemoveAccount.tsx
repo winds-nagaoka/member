67ecf8103f3a70f6b88a84258c2ec8bd5e4c5cb8
ryo4004 <kato@ryo4004.net>
2023-01-30 23:00:53 +0900

    アカウントを削除する

--- src/features/setting/components/RemoveAccount.tsx
+++ src/features/setting/components/RemoveAccount.tsx
@@ -1,10 +1,13 @@
 import clsx from 'clsx'
 import { z } from 'zod'
+import { confirmAlert } from 'react-confirm-alert'
 import { ContentsBox } from '../../../components/ContentsBox'
 import { Form } from '../../../components/Form'
 import { ContentsSubmitButton } from '../../../components/Navigations/ContentsButton'
 import { useStyle } from '../../../utilities/useStyle'
+import { useDeleteAccount } from '../api/deleteAccount'
 import styles from './RemoveAccount.module.scss'
+import { Alert } from '../../../components/Alert/Alert'
 
 type RemoveAccountInput = {
   password: string
@@ -16,8 +19,21 @@ const validationScheme = z.object({
 
 export const RemoveAccount = () => {
   const pc = useStyle()
+  const deleteAccountMutation = useDeleteAccount()
 
-  const onSubmit = () => console.log('onSubmit')
+  const onSubmit = (value: RemoveAccountInput) => {
+    confirmAlert({
+      customUI: ({ onClose }) => (
+        <Alert
+          title="アカウントを削除しますか？"
+          message="ユーザー情報は完全に削除されます。"
+          onConfirm={() => deleteAccountMutation.mutate(value)}
+          confirmButtonLabel="削除"
+          onClose={onClose}
+        />
+      ),
+    })
+  }
 
   return (
     <>
