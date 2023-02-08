c50c7ed7b6844c204afed1b68db4ebb277c216c6
ryo4004 <kato@ryo4004.net>
2023-02-08 23:58:48 +0900

    練習の記録一覧を表示する

--- src/features/practice/api/getHistoryList.ts
+++ src/features/practice/api/getHistoryList.ts
@@ -3,8 +3,28 @@ import { APP_API_URL } from '../../../config'
 import { fetchApi } from '../../../library/fetch'
 import { getSession } from '../../../utilities/session'
 
-const getHistoryList = async () => {
-  return await fetchApi(`${APP_API_URL}/api/reference`, { session: getSession() })
+type HistoryListResponse = {
+  status: boolean
+  list: {
+    id: string
+    time: number
+    detail: {
+      id: string
+      recordStatus: boolean
+      time: {
+        timestamp: boolean
+        date: string
+        time: string
+      }
+      place: string[]
+      label: string
+    }
+    _id: string
+  }[]
+}
+
+const getHistoryList = async (): Promise<HistoryListResponse> => {
+  return await fetchApi(`${APP_API_URL}/api/practice`, { session: getSession() })
 }
 
 export const useHistoryList = () => {
--- src/features/practice/components/HistoryList.tsx
+++ src/features/practice/components/HistoryList.tsx
@@ -1,7 +1,11 @@
-import { ContentsLoading } from '../../../components/ContentsBox'
+import clsx from 'clsx'
+import { ContentsBox, ContentsLoading, Text, TitleFrame } from '../../../components/ContentsBox'
+import { useStyle } from '../../../utilities/useStyle'
 import { useHistoryList } from '../api/getHistoryList'
+import styles from './HistoryList.module.scss'
 
 export const HistoryList = () => {
+  const pc=useStyle()
   const historyListQuery = useHistoryList()
 
   if (historyListQuery.isLoading) {
@@ -12,5 +16,45 @@ export const HistoryList = () => {
     return null
   }
 
-  return <>HistoryList</>
+  return (
+    <ContentsBox>
+      <div className={styles.history}>
+        <ul>
+          {historyListQuery.data.list.length === 0 && (
+            <TitleFrame>
+              <Text>みつかりませんでした</Text>
+            </TitleFrame>
+          )}
+          {historyListQuery.data.list.map((historyItem,index) => {
+            const practice = historyItem.detail
+            const playRequest = practice.recordStatus
+              ? () => {} // this.props.practicePlayRequest(practice.id, 0, 0, true)
+              : () => {}
+            const icon = practice.recordStatus ? (
+              <i className="fas fa-play-circle"></i>
+            ) : (
+              <i className="far fa-times-circle"></i>
+            )
+            const date = 'date' in practice.time ? <div className={styles.date}>{practice.time.date}</div> : false
+            const place = 'place' in practice ? <div className={styles.place}>{practice.place}</div> : false
+            const label = 'label' in practice ? <label>{practice.label}</label> : false
+            return (
+              <li
+                key={`history-${index}`}
+                className={clsx(styles["each-history"],{[styles["has-audio"]]:practice.recordStatus},{[styles["no-audio"]]:!practice.recordStatus},styles[pc])}
+                onClick={playRequest}
+              >
+                <div className={styles.icon}>{icon}</div>
+                <div className={styles.info}>
+                  {place}
+                  {date}
+                </div>
+                <div className={styles.label}>{label}</div>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </ContentsBox>
+  )
 }
