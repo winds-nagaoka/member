0dfe09d2fdd052cd0f02947bd7da20a7f73bf261
ryo4004 <kato@ryo4004.net>
2023-02-08 23:16:58 +0900

    ヘッダーの戻るボタンの表示処理を追加

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import clsx from 'clsx'
-import { Link } from 'react-router-dom'
+import { Link, useMatch } from 'react-router-dom'
 import { useStyle } from '../../utilities/useStyle'
 import styles from './Header.module.scss'
 import { ReactComponent as WindsLogo } from '../../assets/logo.svg'
@@ -9,17 +9,58 @@ import { ReactComponent as MenuIcon } from '../../assets/menu.svg'
 import { ReactComponent as BackIcon } from '../../assets/left.svg'
 import { MenuContents } from './MenuContents'
 
+type PreviousPage = {
+  isMatch: boolean
+  path: string
+}
+
 export const Header = () => {
   const pc = useStyle()
   const [menuOpen, setMenuOpen] = useState(false)
-  const hasPreviousPage = false
+
+  const PREVIOUS_PAGE_MAP: PreviousPage[] = [
+    { isMatch: !!useMatch('/practice'), path: '/' },
+    { isMatch: !!useMatch('/practice/source'), path: '/practice' },
+    { isMatch: !!useMatch('/practice/history'), path: '/practice' },
+    { isMatch: !!useMatch('/manager'), path: '/' },
+    { isMatch: !!useMatch('/bbs'), path: '/' },
+    { isMatch: !!useMatch('/bbs/post'), path: '/bbs' },
+    { isMatch: !!useMatch('/archive'), path: '/' },
+    { isMatch: !!useMatch('/archive/overview/:concertId'), path: '/archive' },
+    {
+      isMatch: !!useMatch('/archive/photo/:concertId'),
+      path: '/archive/overview/' + useMatch('/archive/photo/:concertId')?.params.concertId,
+    },
+    {
+      isMatch: !!useMatch('/archive/video/:concertId'),
+      path: '/archive/overview/' + useMatch('/archive/video/:concertId')?.params.concertId,
+    },
+    { isMatch: !!useMatch('/score'), path: '/' },
+    { isMatch: !!useMatch('/score/detail/:id'), path: '/score' },
+    { isMatch: !!useMatch('/score/box'), path: '/score' },
+    { isMatch: !!useMatch('/setting'), path: '/' },
+    { isMatch: !!useMatch('/setting/name'), path: '/setting' },
+    { isMatch: !!useMatch('/setting/email'), path: '/setting' },
+    { isMatch: !!useMatch('/setting/valid/:key'), path: '/setting' },
+    { isMatch: !!useMatch('/setting/password'), path: '/setting' },
+    { isMatch: !!useMatch('/setting/session'), path: '/setting' },
+    { isMatch: !!useMatch('/setting/admin'), path: '/setting' },
+    { isMatch: !!useMatch('/setting/delete'), path: '/setting' },
+    { isMatch: !!useMatch('/setting/score/mail'), path: '/setting' },
+    { isMatch: !!useMatch('/setting/score/admin'), path: '/setting' },
+    { isMatch: !!useMatch('/setting/terms'), path: '/setting' },
+    { isMatch: !!useMatch('/setting/about'), path: '/setting' },
+    { isMatch: !!useMatch('/setting/license'), path: '/setting' },
+  ]
+  const previousPage = PREVIOUS_PAGE_MAP.find((value) => value.isMatch) ?? null
+
   return (
     <div className={styles['navigation-header']}>
       <div className={clsx(styles.header, styles[pc])}>
         <div className={styles.logo}>
           <WindsLogo />
         </div>
-        <LeftNavigations hasPreviousPage={hasPreviousPage} onMenuOpen={() => setMenuOpen(true)} />
+        <LeftNavigations previousPage={previousPage} onMenuOpen={() => setMenuOpen(true)} />
         <RightNavigations />
       </div>
       <div
@@ -33,15 +74,21 @@ export const Header = () => {
   )
 }
 
-const LeftNavigations = ({ hasPreviousPage, onMenuOpen }: { hasPreviousPage: boolean; onMenuOpen: () => void }) => {
+const LeftNavigations = ({
+  previousPage,
+  onMenuOpen,
+}: {
+  previousPage: PreviousPage | null
+  onMenuOpen: () => void
+}) => {
   const pc = useStyle()
 
   return (
     <>
-      {pc === 'mobile' && hasPreviousPage && (
+      {pc === 'mobile' && previousPage && (
         <>
           <div className={clsx(styles.label, styles.back)}>
-            <Link to="/">
+            <Link to={previousPage.path}>
               <div className={styles['back-icon']}>
                 <BackIcon />
               </div>
@@ -50,7 +97,7 @@ const LeftNavigations = ({ hasPreviousPage, onMenuOpen }: { hasPreviousPage: boo
           </div>
         </>
       )}
-      {pc === 'mobile' && !hasPreviousPage && (
+      {pc === 'mobile' && !previousPage && (
         <div className={styles.label} onClick={onMenuOpen}>
           <MenuIcon />
         </div>
