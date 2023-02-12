a8d144625c5001307c41912f173aa4942cd49702
ryo4004 <kato@ryo4004.net>
2023-02-12 12:46:58 +0900

    アカウントの登録処理を修正

--- src/features/public/components/RegisterForm.tsx
+++ src/features/public/components/RegisterForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import z from 'zod'
 import { Button, Form, Input } from '../../../components/Form'
@@ -19,51 +20,75 @@ type RegisterInput = {
 
 export const RegisterForm = ({ onSuccess }: { onSuccess: () => void }) => {
   const pc = useStyle()
+  const [step, setStep] = useState<'first' | 'second'>('first')
   const { register, isLoggingIn: isLoading } = useAuth()
 
   return (
     <div className={styles.form}>
       <h2 className={styles[pc]}>新規登録</h2>
-      <div className={styles.text}>共通パスワードを入力してください</div>
       <Form<RegisterInput>
         onSubmit={async (values) => {
           const user = await register(values)
           user && onSuccess()
         }}
         validationScheme={validationScheme}
+        options={{ mode: 'all' }}
       >
         {({ register, formState }) => {
-          const hasError = !formState.isValid
+          formState.isSubmitted && setStep('first')
           return (
             <>
-              <Input
-                type="text"
-                label="会員専用パスワード"
-                registration={register('passKey')}
-                error={formState.errors['passKey']}
-              />
-              <Input
-                type="text"
-                label="ユーザー名"
-                registration={register('userId')}
-                error={formState.errors['userId']}
-              />
-              <Input
-                type="password"
-                label="パスワード"
-                registration={register('password')}
-                error={formState.errors['password']}
-              />
-              <div className={styles.links}>
-                <div className={styles.link}>
-                  <Link to="/login">ログインはこちら</Link>
-                </div>
-                <div className={styles.button}>
-                  <Button type="submit" isLoading={isLoading} disabled={hasError}>
-                    登録
-                  </Button>
-                </div>
-              </div>
+              {step === 'first' && (
+                <>
+                  <div className={styles.text}>共通パスワードを入力してください</div>
+                  <Input
+                    type="text"
+                    label="会員専用パスワード"
+                    registration={register('passKey')}
+                    error={formState.errors['passKey']}
+                  />
+                  <div className={styles.links}>
+                    <div className={styles.link}>
+                      <Link to="/login">ログインはこちら</Link>
+                    </div>
+                    <div className={styles.button}>
+                      <Button
+                        disabled={!!formState.errors['passKey'] || !formState.isDirty}
+                        onClick={() => setStep('second')}
+                      >
+                        次へ
+                      </Button>
+                    </div>
+                  </div>
+                </>
+              )}
+              {step === 'second' && (
+                <>
+                  <div className={styles.text}>ユーザー名とパスワードは自由に指定できます</div>
+                  <Input
+                    type="text"
+                    label="ユーザー名"
+                    registration={register('userId')}
+                    error={formState.errors['userId']}
+                  />
+                  <Input
+                    type="password"
+                    label="パスワード"
+                    registration={register('password')}
+                    error={formState.errors['password']}
+                  />
+                  <div className={styles.links}>
+                    <div className={styles.link}>
+                      <Link to="/login">ログインはこちら</Link>
+                    </div>
+                    <div className={styles.button}>
+                      <Button type="submit" isLoading={isLoading} disabled={!formState.isValid}>
+                        登録
+                      </Button>
+                    </div>
+                  </div>
+                </>
+              )}
             </>
           )
         }}
