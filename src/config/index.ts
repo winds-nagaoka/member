a1a86f2b4a158f7abfb55a32df872904ae68377d
ryo4004 <kato@ryo4004.net>
2023-02-12 13:16:48 +0900

    envファイルを使う

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
-export const AUTH_API_URL = 'http://localhost:3003'
-export const APP_API_URL = 'http://localhost:3007'
-export const SCORE_API_URL = 'http://localhost:3005'
-export const API_PATH = 'https://api.winds-n.com'
+export const AUTH_API_URL = process.env.REACT_APP_AUTH_API_URL as string
+export const APP_API_URL = process.env.REACT_APP_APP_API_URL as string
+export const SCORE_API_URL = process.env.REACT_APP_SCORE_API_URL as string
+export const API_PATH = process.env.REACT_APP_API_PATH as string
 export const VERSION = process.env.REACT_APP_VERSION as string
