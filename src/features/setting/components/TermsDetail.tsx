65ca79859c84cf1f06ca9264136d94c6dba7bd27
ryo4004 <kato@ryo4004.net>
2024-04-26 21:01:29 +0900

    取得した規約を表示する

--- src/features/setting/components/TermsDetail.tsx
+++ src/features/setting/components/TermsDetail.tsx
@@ -1,4 +1,4 @@
-import { ContentsBox } from '../../../components/ContentsBox'
+import { ContentsBox, ContentsLoading, Text } from '../../../components/ContentsBox'
 import { ReactComponent as Logo } from '../../../assets/hr.svg'
 
 import styles from './TermsDetail.module.scss'
@@ -24,11 +24,37 @@ export const TermsDetail = () => {
             </div>
           </div>
         </div>
-        {windsTermsQuery.isLoading && <>読み込み中</>}
-        {windsTermsQuery.data?.split('\n').map((line,index) => (
-          <div key={index}>{line}</div>
-        ))}
+        {windsTermsQuery.isLoading && <ContentsLoading />}
+        {windsTermsQuery.data && (
+          <Text>
+            <div className={styles.monospaced}>
+              {windsTermsQuery.data.split('\n').map((line, index) => (
+                <TermsLine key={index}>{line}</TermsLine>
+              ))}
+            </div>
+          </Text>
+        )}
       </div>
     </ContentsBox>
   )
+}
+
+const TermsLine = ({ children }: { children: string }) => {
+  if (children === '') {
+    return <div>&nbsp;</div>
+  }
+
+  if (/^（.*）/.test(children)) {
+    return (
+      <div className={styles['sub-title']}>
+        <h3>{children}</h3>
+      </div>
+    )
+  }
+
+  if (/^付則$/.test(children.trim())) {
+    return <div className={styles.additional}>{children}</div>
+  }
+
+  return <div>{children}</div>
 }
